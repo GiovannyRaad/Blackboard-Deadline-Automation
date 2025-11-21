6972c3d011d482851086b5a958a436171b05968e
GiovannyRaad <213656413+GiovannyRaad@users.noreply.github.com>
2025-11-21 20:35:39 +0200

    Access the blackboard calendar page

--- eventsFetcher.py
+++ eventsFetcher.py
@@ -1,5 +1,9 @@
 import requests
+import base64
 from bs4 import BeautifulSoup
+import os
+username = os.getenv("UNI_USER")
+password = os.getenv("UNI_PASS")
 
 class APIClient:
     def __init__(self, base_url, headers=None):
@@ -14,25 +18,26 @@ class APIClient:
         return f"{self.base_url}/{path.lstrip('/')}"
 
     def get(self, path, **kwargs):
-        resp = self.session.get(self._url(path), **kwargs)
+        resp = self.session.get(path, **kwargs)
         resp.raise_for_status()
-        return resp.text
+        return resp
 
     def post(self, path, data=None, **kwargs):
         resp = self.session.post(path, data=data, **kwargs)
         resp.raise_for_status()
-        return resp.text
+        return resp
 
     def put(self, path, data=None, **kwargs):
         resp = self.session.put(self._url(path), data=data, **kwargs)
         resp.raise_for_status()
-        return resp.text
+        return resp
 
     def delete(self, path, **kwargs):
         resp = self.session.delete(self._url(path), **kwargs)
         resp.raise_for_status()
         return resp.status_code
     
+#Initial page
 
 url = "https://elearn.lau.edu.lb/auth-saml/saml/login"
 
@@ -43,12 +48,52 @@ params = {
 
 client = APIClient(url)
 
-response = client.get("", params=params)
+response = client.get("https://elearn.lau.edu.lb/", allow_redirects=False)
 
-soup = BeautifulSoup(response, "html.parser")
+response = client.get(url, params=params, allow_redirects=False)
+
+print(response.headers.get("Location"))
+
+text = response.text
+
+#Extracting the SAMLRequest value from the response HTML
+
+soup = BeautifulSoup(text, "html.parser")
 SAMLRequest = soup.find("input", {"name": "SAMLRequest"})["value"]
 
 payload = { "SAMLRequest": SAMLRequest }
 
-response = client.post("https://iam.lau.edu.lb/isam/sps/LAUIDP/saml20/login", data=payload)
-print(response)
+response = client.post("https://iam.lau.edu.lb/isam/sps/LAUIDP/saml20/login", data=payload, allow_redirects=False)
+
+print(response.headers.get("Location"))
+
+
+#Submitting login form
+
+payload = { "username": username, "password": password ,
+           "logonID": "", "login-form-type": "pwd" }
+
+headers = {
+    "referer": "https://iam.lau.edu.lb/isam/sps/LAUIDP/saml20/login"
+}
+response = client.post("https://iam.lau.edu.lb/pkmslogin.form", data=payload, allow_redirects=False, headers=headers)
+
+#Simulate js , username and password encode
+#Need to update the cookies with the encoded values and send a get request
+
+encoded_username = base64.b64encode(username.encode()).decode()
+client.session.cookies.update({
+    "TM-SESSION-UN": encoded_username,
+    "BR-SESSION-UN": encoded_username,
+    "PC-SESSION-UN": encoded_username  
+})
+
+response = client.post("https://iam.lau.edu.lb/pkmslogin.form", data=payload, allow_redirects=False, headers=headers)
+
+#response = client.get("https://iam.lau.edu.lb/isam/sps/LAUIDP/saml20/login", headers={"referer":"https://iam.lau.edu.lb/isam/sps/LAUIDP/saml20/login"})
+
+#Request calendar page
+url = "https://elearn.lau.edu.lb/ultra/calendar"
+
+response = client.get(url)
+print(response.text)
